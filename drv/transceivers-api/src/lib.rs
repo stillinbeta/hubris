eba4f9f25d11779696f4da69c65aee81f03f3434
Aaron Hartwig <aaron@oxidecomputer.com>
2022-09-07 17:34:43 -0500

    start skeleton of transceivers api and server

---
+++ drv/transceivers-api/src/lib.rs
@@ -0,0 +1,12 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+//! API crate for QSFP transceiver managment
+
+#![no_std]
+
+use derive_idol_err::IdolError;
+use userlib::*;
+
+include!(concat!(env!("OUT_DIR"), "/client_stub.rs"));
---
+++ drv/transceivers-server/src/main.rs
@@ -0,0 +1,28 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#![no_std]
+#![no_main]
+
+use userlib::*;
+
+// NOTE: you will probably want to remove this when you write your actual code;
+// we need to import userlib to get this to compile, but it throws a warning
+// because we're not actually using it yet!
+#[allow(unused_imports)]
+use userlib::*;
+
+#[export_name = "main"]
+fn main() -> ! {
+    loop {
+        // NOTE: you need to put code here before running this! Otherwise LLVM
+        // will turn this into a single undefined instruction.
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+mod idl {
+    include!(concat!(env!("OUT_DIR"), "/server_stub.rs"));
+}
